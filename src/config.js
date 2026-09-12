12f496f589745ebc01f5f8b2e617fb57f94021da
Pratham Vashist <nc818350@gmail.com>
2026-09-12 18:46:52 +0530

    Ready for deployment

---
+++ src/config.js
@@ -0,0 +1,19 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAi3dcKPdqUIzJ9BLopzVFncsUqUeLvu_Q",
+  authDomain: "netflix-clone-71935.firebaseapp.com",
+  projectId: "netflix-clone-71935",
+  storageBucket: "netflix-clone-71935.firebasestorage.app",
+  messagingSenderId: "672071598472",
+  appId: "1:672071598472:web:bdc7bcc30fa0ec18297a3d"
+};
+
+
+
+// craete a/c on themoviedb.org and replace access token below
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTNlNzczZDIwY2M2Y2NhNWQ4YWVjMjQzNTdlNDc1ZCIsInN1YiI6IjY2MjIyMDFhYWUzODQzMDE4NzJhNTJjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vPDW2QwNr9hIRdOvJx_x8hbHnDYZGHMtnZwfkqb3J8U";
+
+
+  export {firebaseConfig, TMDB_Access_Key};
